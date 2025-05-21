67b94099286e73a48a09c403821e37fc6cdd31b4
Md rashid Ali <raashid.ali0@gmail.com>
2025-05-22 00:07:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-zssg.onrender.com",
     credentials: true,
   })
 );
